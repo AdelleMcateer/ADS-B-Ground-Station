b6a3cbf61028c9fde7abee880331b110f9614126
AdelleMcateer <adellemaca@gmail.com>
2020-12-12 11:42:57 +0000

    Simple test file to display the json data on the PI terminal

--- Python/flightdatatest.py
+++ Python/flightdatatest.py
@@ -1,1 +1,12 @@
+# Author: Adelle McAteer
+# Version: 1
+# Date: 12-10-2020
+
+# A test script to import the json data from the PiAware feed
+
+from urllib.request import urlopen
+
+import json
+json = json.loads(urlopen('http://192.168.1.123/dump1090-fa/data/aircraft.json').read())
+print (json)
 
