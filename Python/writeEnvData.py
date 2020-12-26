b841701b261ebf22034e913d7b57facd0a2f310a
AdelleMcateer <adellemaca@gmail.com>
2020-12-26 16:36:57 +0000

    Updated flighttest.py to display a message on the SenseHat Added writeEnvData.py to send a react to ThingSpeak

--- Python/writeEnvData.py
+++ Python/writeEnvData.py
@@ -2,27 +2,42 @@
 
 # Author: Adelle McAteer
 # Version: 1
-# Date: 12-26-2020
+# Date: 12-24-2020
 
-# This script runs through the PiAware Array and posts a message to the sense hat
+# This script incorporates the flighttest.py script.
+# It is expanded further to send a react to ThingSpeak if data is detected in the array 
+
+from urllib.request import urlopen
+import  json
+import  time
+from sense_hat import SenseHat
 
 import requests
 import json
 
-from time import sleep
-import time
-from sense_hat import SenseHat
+#ThingSpeak API PiAware channel key
+WRITE_API_KEY='8I224HGCPZE8CG6Y'
+
+#ThingSpeak ThingTweetURL & channel API key
+baseURL='https://api.thingspeak.com/update?api_key=%s' % WRITE_API_KEY
 
 sense = SenseHat()
 
 #clear sensehat and intialise light_state
 sense.clear()
 
-green = (0, 255, 0)
-red = (255,0,0)
-
 #api -endpoint
 URL = 'http://192.168.1.123/dump1090-fa/data/aircraft.json'
+
+green =(0, 255, 0)
+red = (255, 0, 0)
+
+def writeData(status):
+    # Sending the data to thingspeak in the query string
+    conn = urlopen(baseURL + '&field1=%s' % (status))
+    print(conn.read())
+    # Closing the connection
+    conn.close()
 
 # If statement to search the json array
 # The sense hat displays Data Detection or No data depending on the information
@@ -37,6 +52,9 @@ while True:
 
     if flightData:
         sense.show_message("Data Detection!", text_colour = green)
+        status=1
+        writeData(status)
+
     else:
       # if not aircraft :
         sense.show_message("No Data", text_colour = red)
